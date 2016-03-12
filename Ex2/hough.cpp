f73056d88c5578dd1cd2a59bde43b88752553284
XuYongzhe <503262037@qq.com>
2016-03-12 11:03:57 +0800

    adjust file

---
+++ Ex2/hough.cpp
@@ -0,0 +1,7 @@
+#include "CImg.h"
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#include <string>
+using namespace cimg_library;
+using namespace std;
--- run.sh
+++
@@ -1,1 +0,0 @@
- g++ -o filter.out filter.cpp -O2 -lm -lpthread -I/usr/X11R6/include -L/usr/X11R6/lib -lm -lpthread -lX11
